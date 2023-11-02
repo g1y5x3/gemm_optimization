e50db814540dc15808fce35e266c04552c992b41
Yixiang Gao <yg5d6@mail.missouri.edu>
2023-11-02 12:57:40 -0500

    GEMM dumb kernel complete

--- handcode_GEMM_opt.py
+++ handcode_GEMM_opt.py
@@ -7,6 +7,7 @@ from tinygrad.features.search import time_linearizer
 from tinygrad.graph import print_tree
 from tinygrad.lazy import vars_from_ast
 from tinygrad.shape.symbolic import sym_infer
+from tinygrad.helpers import ansilen
 
 if __name__ == "__main__":
   # Define matrices A, B
@@ -23,32 +24,24 @@ if __name__ == "__main__":
   print(f"Matrix A: {A_tiny}")
   print(f"Matrix B: {B_tiny}")
 
+  # three kernels are involved, the first two kernels are LoadOps and the last kernel is performing GEMM
   seen = set()
   out = A_tiny @ B_tiny
   sched = out.lazydata.schedule(seen)
-
-  # the first two kernels are LoadOps and the last kernel is performing GEMM
-  print(f"Total number of kernels: {len(sched)}")
   print(f"kernel schedule:")
   print_tree(sched[2].ast)
 
   # create buffers
   rawbufs = [device.buffer(sched[2].out.st.size(), sched[2].out.dtype)] + [device.buffer(x.st.size(), x.dtype) for x in sched[2].inputs]
 
-  # 0. without any optimization (dump kernel)
   lin = Linearizer(sched[2].ast, device.linearizer_opts)
-  lin.linearize()
+  lin.linearize()   # 0. without any optimization (dump kernel)
   lin.printbufs()
+
   prg = cast(Compiled, Device[Device.DEFAULT]).to_program(lin)
+  print(prg.prg)
 
-  # tm = time_linearizer(lin, rawbufs, allow_test_size=False, cnt=10)
-  # gflops = sym_infer(lin.info.flops, {k:k.min for k in vars_from_ast(lin.ast)})*1e-9/tm
-  
-  # print(f"                 kernel {2:2d} {lin.display_name+' '*(37-ansilen(lin.display_name))} {str(lin.global_size):18s} {str(lin.local_size):12s} takes {tm*1000:7.2f} ms, {gflops:6.0f} GFLOPS")
-  # print(f"                 kernel {2:2d}  {str(lin.global_size):18s} {str(lin.local_size):12s} takes {tm*1000:7.2f} ms, {gflops:6.0f} GFLOPS")
-  # 1. naive kernel
+  tm = time_linearizer(lin, rawbufs, allow_test_size=False, cnt=10)
+  gflops = sym_infer(lin.info.flops, {k:k.min for k in vars_from_ast(lin.ast)})*1e-9/tm
 
-  # for i in range(5):
-    # C_tiny = (A_tiny@B_tiny).realize()
-
-  # print(C_tiny)
+  print(f"*** {tm*1000:7.2f} ms : kernel {lin.display_name+' '*(37-ansilen(lin.display_name))} {str(lin.global_size):18s} {str(lin.local_size):12s} takes {tm*1000:7.2f} ms, {gflops:6.0f} GFLOPS")
