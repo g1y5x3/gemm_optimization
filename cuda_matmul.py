9fa0f5694e358ccfd2eedec68c177d75f6a19f08
Yixiang Gao <yg5d6@mail.missouri.edu>
2023-11-02 18:51:10 -0500

    finish naive kernel

--- cuda_matmul.py
+++ cuda_matmul.py
@@ -1,16 +1,6 @@
 import numpy as np
+from tinygrad.helpers import getenv
 from tinygrad.runtime.ops_cuda import RawCUDABuffer, CUDAProgram, compile_cuda
-
-N = 4096
-
-A = np.random.randn(4096, 4096).astype(np.float32) 
-B = np.random.randn(4096, 4096).astype(np.float32)
- 
-a = RawCUDABuffer.fromCPU(A)
-b = RawCUDABuffer.fromCPU(B)
-c = RawCUDABuffer.fromCPU(np.zeros((N,N),dtype=np.float32))
-
-FLOPS = N*N*N*2
 
 kernel0 = """
 __global__ void gemm(const float* A, const float* B, float* C) {
@@ -26,7 +16,7 @@ __global__ void gemm(const float* A, const float* B, float* C) {
 }
 """
 
-kernel1 = """
+kernel1_1 = """
 __global__ void gemm(const float* A, const float* B, float* C) {
   int x = blockIdx.x * blockDim.x + threadIdx.x;
   int y = blockIdx.y * blockDim.y + threadIdx.y;
@@ -40,25 +30,43 @@ __global__ void gemm(const float* A, const float* B, float* C) {
 }
 """
 
-print(kernel0)
-prog = CUDAProgram("gemm", compile_cuda(kernel0))
+kernel1_2 = """
+__global__ void gemm(const float* A, const float* B, float* C) {
+  int x = blockIdx.y * blockDim.y + threadIdx.y;
+  int y = blockIdx.x * blockDim.x + threadIdx.x;
+  float tmp = 0.0f;
+  for (int i = 0; i < 4096; ++i) {
+    float val0 = A[(x*4096) + i]; 
+    float val1 = B[y+(i*4096)]; 
+    tmp = ((val0*val1)) + tmp;
+  }
+  C[(x*4096)+y] = tmp;
+}
+"""
 
-local_dim = [1, 1, 1]
-global_dim = [4096, 4096, 1]
+def run_kernel(name, kernel, local_dim, global_dim):
 
-tm = min([prog(global_dim, local_dim, a, b, c, wait=True) for _ in range(10)])
-print(f"dumb kernel      {str(global_dim):18s} {str(local_dim):12s} takes {tm*1000:7.2f} ms, {FLOPS*1e-9/tm:6.0f} GFLOPS matmul")
+  N = 4096
 
-np.testing.assert_allclose(A @ B, c.toCPU().reshape((N,N)), atol=1e-3, rtol=1e-3)
+  A = np.random.randn(4096, 4096).astype(np.float32) 
+  B = np.random.randn(4096, 4096).astype(np.float32)
+   
+  a = RawCUDABuffer.fromCPU(A)
+  b = RawCUDABuffer.fromCPU(B)
+  c = RawCUDABuffer.fromCPU(np.zeros((N,N),dtype=np.float32))
 
-print(kernel1)
-prog = CUDAProgram("gemm", compile_cuda(kernel1))
+  FLOPS = N*N*N*2
 
-local_dim = [32, 32, 1]
-global_dim = [128, 128, 1]
+  if getenv("DEBUG", 0) > 0:
+    print(kernel)
 
-tm = min([prog(global_dim, local_dim, a, b, c, wait=True) for _ in range(10)])
-print(f"naive kernel     {str(global_dim):18s} {str(local_dim):12s} takes {tm*1000:7.2f} ms, {FLOPS*1e-9/tm:6.0f} GFLOPS matmul")
+  prog = CUDAProgram("gemm", compile_cuda(kernel))
 
-np.testing.assert_allclose(A @ B, c.toCPU().reshape((N,N)), atol=1e-3, rtol=1e-3)
+  tm = min([prog(global_dim, local_dim, a, b, c, wait=True) for _ in range(1)])
+  print(f"{name}    {str(global_dim):18s} {str(local_dim):12s} takes {tm*1000:7.2f} ms, {FLOPS*1e-9/tm:6.0f} GFLOPS")
+  
+  np.testing.assert_allclose(A @ B, c.toCPU().reshape((N,N)), atol=1e-3, rtol=1e-3)
 
+run_kernel("dumb kernel    ", kernel0,   [1, 1, 1],   [4096, 4096, 1])
+run_kernel("naive kernel v1", kernel1_1, [32, 32, 1], [128, 128, 1])
+run_kernel("naive kernel v2", kernel1_2, [32, 32, 1], [128, 128, 1])
--- handcode_GEMM_opt.py
+++ handcode_GEMM_opt.py
@@ -7,7 +7,21 @@ from tinygrad.features.search import time_linearizer
 from tinygrad.graph import print_tree
 from tinygrad.lazy import vars_from_ast
 from tinygrad.shape.symbolic import sym_infer
-from tinygrad.helpers import ansilen
+from tinygrad.helpers import ansilen, getenv
+from tinygrad.codegen.kernel import Opt, OptOps
+
+def run_kernel(name, lin, rawbufs):
+  lin.linearize()
+  prg = cast(Compiled, Device[Device.DEFAULT]).to_program(lin)
+
+  if getenv("DEBUG",0) > 0: 
+    lin.printbufs()
+    print(prg.prg)
+
+  tm = time_linearizer(lin, rawbufs, allow_test_size=False, cnt=10)
+  gflops = sym_infer(lin.info.flops, {k:k.min for k in vars_from_ast(lin.ast)})*1e-9/tm
+
+  print(f"{name}     {lin.display_name+' '*(37-ansilen(lin.display_name))} {str(lin.global_size):18s} {str(lin.local_size):12s} takes {tm*1000:7.2f} ms, {gflops:6.0f} GFLOPS")
 
 if __name__ == "__main__":
 
@@ -16,32 +30,31 @@ if __name__ == "__main__":
   
   # the device being optimized for
   device: Compiled = Device[Device.DEFAULT]
-  print(f"optimizing for {Device.DEFAULT}")
 
   A_tiny = Tensor(A)
   B_tiny = Tensor(B)
-
-  print(f"Matrix A: {A_tiny}")
-  print(f"Matrix B: {B_tiny}")
 
   # three kernels are involved, the first two kernels are LoadOps and the last kernel is performing GEMM
   seen = set()
   out = A_tiny @ B_tiny
   sched = out.lazydata.schedule(seen)
-  print(f"kernel schedule:")
-  print_tree(sched[2].ast)
+  if getenv("DEBUG",0) > 0:
+    print(f"optimizing for {Device.DEFAULT}")
+    print(f"Matrix A: {A_tiny}")
+    print(f"Matrix B: {B_tiny}")
+    print(f"kernel schedule:")
+    print_tree(sched[2].ast)
 
   # create buffers
   rawbufs = [device.buffer(sched[2].out.st.size(), sched[2].out.dtype)] + [device.buffer(x.st.size(), x.dtype) for x in sched[2].inputs]
 
+  # without any optimization - dump kernel
   lin = Linearizer(sched[2].ast, device.linearizer_opts)
-  lin.linearize()   # 0. without any optimization (dump kernel)
-  lin.printbufs()
+  run_kernel("dumb kernel ", lin, rawbufs)
 
-  prg = cast(Compiled, Device[Device.DEFAULT]).to_program(lin)
-  print(prg.prg)
+  lin = Linearizer(sched[2].ast, device.linearizer_opts)
+  lin.apply_opt(Opt(op=OptOps.LOCAL, axis=0, amt=32))
+  lin.apply_opt(Opt(op=OptOps.LOCAL, axis=1, amt=32))
 
-  tm = time_linearizer(lin, rawbufs, allow_test_size=False, cnt=10)
-  gflops = sym_infer(lin.info.flops, {k:k.min for k in vars_from_ast(lin.ast)})*1e-9/tm
+  run_kernel("naive kernel", lin, rawbufs)
 
-  print(f"dumb kernel {lin.display_name+' '*(37-ansilen(lin.display_name))} {str(lin.global_size):18s} {str(lin.local_size):12s} takes {tm*1000:7.2f} ms, {gflops:6.0f} GFLOPS")
