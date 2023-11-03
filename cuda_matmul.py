18f73d622323a718a232e8cf84f26ffc5dd50804
Yixiang Gao <yg5d6@mail.missouri.edu>
2023-11-03 12:44:15 -0500

    repeat each kernel 10 times

--- cuda_matmul.py
+++ cuda_matmul.py
@@ -2,10 +2,24 @@ import numpy as np
 from tinygrad.helpers import getenv
 from tinygrad.runtime.ops_cuda import RawCUDABuffer, CUDAProgram, compile_cuda
 
-kernel0 = """
+kernel0_1 = """
 __global__ void gemm(const float* A, const float* B, float* C) {
   int x = blockIdx.x;
   int y = blockIdx.y;
+  float tmp = 0.0f;
+  for (int i = 0; i < 4096; ++i) {
+    float val0 = A[(x*4096) + i]; 
+    float val1 = B[y+(i*4096)]; 
+    tmp = ((val0*val1)) + tmp;
+  }
+  C[(x*4096)+y] = tmp;
+}
+"""
+
+kernel0_2 = """
+__global__ void gemm(const float* A, const float* B, float* C) {
+  int x = blockIdx.y;
+  int y = blockIdx.x;
   float tmp = 0.0f;
   for (int i = 0; i < 4096; ++i) {
     float val0 = A[(x*4096) + i]; 
@@ -62,11 +76,12 @@ def run_kernel(name, kernel, local_dim, global_dim):
 
   prog = CUDAProgram("gemm", compile_cuda(kernel))
 
-  tm = min([prog(global_dim, local_dim, a, b, c, wait=True) for _ in range(1)])
+  tm = min([prog(global_dim, local_dim, a, b, c, wait=True) for _ in range(10)])
   print(f"{name}    {str(global_dim):18s} {str(local_dim):12s} takes {tm*1000:7.2f} ms, {FLOPS*1e-9/tm:6.0f} GFLOPS")
   
   np.testing.assert_allclose(A @ B, c.toCPU().reshape((N,N)), atol=1e-3, rtol=1e-3)
 
-run_kernel("dumb kernel    ", kernel0,   [1, 1, 1],   [4096, 4096, 1])
+run_kernel("dumb kernel v1 ", kernel0_1, [1, 1, 1],   [4096, 4096, 1])
+run_kernel("dumb kernel v2 ", kernel0_2, [1, 1, 1],   [4096, 4096, 1])
 run_kernel("naive kernel v1", kernel1_1, [32, 32, 1], [128, 128, 1])
 run_kernel("naive kernel v2", kernel1_2, [32, 32, 1], [128, 128, 1])
